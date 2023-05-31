79e48101e0809d560e1a1d18c894b5207f1addb2
Janay Ma <janay.ma7@gmail.com>
2023-05-31 15:51:53 -0700

    comment out console logs

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 
 function sum(fromN, toN) {
-  // Sum all the values from fromN up to toN
+  if (fromN === toN) {
+   // console.log(fromN, toN)
+    return fromN;
+  }
+  //console.log(fromN, toN)
+  return fromN + sum(fromN+1, toN);
 }
 
 module.exports = sum;
+
